832f5ec909d30e79f2b64a64a818c7ddf84b3df2
PremasaiK <81842544+PremasaiK@users.noreply.github.com>
2022-08-10 17:31:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const requestHandler = (request, response) => {
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
-    response.end('AchiStar Technologies-Welocome !!!');
+    response.end('AchiStar Technologies-Welcome !!!');
 };
 
 const server = http.createServer(requestHandler);
